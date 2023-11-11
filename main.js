0d02598759d6e148d386e11887ab4c92bbbc7138
SesanDaniel <obisesan.seyid@gmail.com>
2023-11-11 17:00:34 +0400

    Vue Event Handling

--- main.js
+++ main.js
@@ -11,5 +11,20 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        displayColor(variantImage) {
+            this.image = variantImage;
+        },
+        removeFromCart() {
+            if (this.cart > 0) {
+                this.cart -= 1
+            }
+            
+        }
     }
 })
